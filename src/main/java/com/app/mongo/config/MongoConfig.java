b38e01a7a2c9f0aa3966293fe2405558c30e14a4
sandeepreddy1945 <b.sandeepreddy209@gmail.com>
2018-07-18 21:48:43 +0530

    Signed-off-by: sandeepreddy1945 <b.sandeepreddy209@gmail.com>

--- src/main/java/com/app/mongo/config/MongoConfig.java
+++ src/main/java/com/app/mongo/config/MongoConfig.java
@@ -1,7 +1,10 @@
 package com.app.mongo.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 import com.mongodb.MongoClient;
@@ -9,22 +12,24 @@ import com.mongodb.MongoClient;
 @Configuration
 public class MongoConfig extends AbstractMongoConfiguration {
 
-	@Value(value = "${application.server.name}")
-	private String dbName;
+    @Value(value = "${application.server.name}")
+    private String dbName;
 
-	@Value(value = "${application.server.host}")
-	private String dbHost;
+    @Value(value = "${application.server.host}")
+    private String dbHost;
 
-	@Value(value = "${application.server.port}")
-	private String dbPort;
+    @Value(value = "${application.server.port}")
+    private String dbPort;
 
-	@Override
-	protected String getDatabaseName() {
-		return dbName;
-	}
+    @Override
+    protected String getDatabaseName() {
+        return dbName;
+    }
 
-	@Override
-	public MongoClient mongoClient() {
-		return new MongoClient(dbHost, Integer.valueOf(dbPort));
-	}
+    @Override
+    @Bean
+    @Scope(scopeName = ConfigurableBeanFactory.SCOPE_SINGLETON)
+    public MongoClient mongoClient() {
+        return new MongoClient(dbHost, Integer.valueOf(dbPort));
+    }
 }
